925bd3cf85e8c3626be7d27187426aad9815c024
tobyxie0212 <tobyxie0212@163.com>
2015-08-27 11:23:04 +1200

    modified move_base.xml and base_local_planner_params_*.yaml for better local-planner selector

--- auckbot_navigation/launch/planner_setup_selector.sh
+++ auckbot_navigation/launch/planner_setup_selector.sh
@@ -2,7 +2,7 @@
 # planner setup selector
 
 PS3='Please enter your choice: '
-options=("global: NavFN" "global: Carrot Planner" "global: Global Planner Grid" "global: Opti Planner" "local: EDWA Planner" "local: DWA Planner")
+options=("global: NavFN" "global: Carrot Planner" "global: Global Planner Grid" "global: Opti Planner" "local: EDWA Planner" "local: EODWA Planner" "local: DWA Planner")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -31,13 +31,30 @@ do
             break
             ;;
         "local: EDWA Planner")
-            export MB_BASE_LOCAL_PLANNER_E='true'
-            echo "local planner EDWA: $MB_BASE_LOCAL_PLANNER_E"
+            export MB_BASE_LOCAL_PLANNER_EDWA='true'
+            export MB_BASE_LOCAL_PLANNER_EODWA='false'
+            export MB_BASE_LOCAL_PLANNER_DWA='false'
+            echo "local planner EDWA: $MB_BASE_LOCAL_PLANNER_EDWA"
+            echo "local planner EODWA: $MB_BASE_LOCAL_PLANNER_EODWA"
+            echo "local planner DWA: $MB_BASE_LOCAL_PLANNER_DWA"
+            break
+            ;;
+        "local: EODWA Planner")
+            export MB_BASE_LOCAL_PLANNER_EDWA='false'
+            export MB_BASE_LOCAL_PLANNER_EODWA='true'
+            export MB_BASE_LOCAL_PLANNER_DWA='false'
+            echo "local planner EDWA: $MB_BASE_LOCAL_PLANNER_EDWA"
+            echo "local planner EODWA: $MB_BASE_LOCAL_PLANNER_EODWA"
+            echo "local planner DWA: $MB_BASE_LOCAL_PLANNER_DWA"
             break
             ;;
         "local: DWA Planner")
-            export MB_BASE_LOCAL_PLANNER_E='false'
-            echo "local planner EDWA: $MB_BASE_LOCAL_PLANNER_E"
+            export MB_BASE_LOCAL_PLANNER_EDWA='false'
+            export MB_BASE_LOCAL_PLANNER_EODWA='false'
+            export MB_BASE_LOCAL_PLANNER_DWA='true'
+            echo "local planner EDWA: $MB_BASE_LOCAL_PLANNER_EDWA"
+            echo "local planner EODWA: $MB_BASE_LOCAL_PLANNER_EODWA"
+            echo "local planner DWA: $MB_BASE_LOCAL_PLANNER_DWA"
             break
             ;;
         *) echo "invalid option"
